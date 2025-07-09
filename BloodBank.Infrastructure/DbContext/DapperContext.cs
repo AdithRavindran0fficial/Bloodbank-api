0366c7630a8056847b4e91b81194bbf29f0c21e2
Adith <adithravindranofficial@gmail.com>
2025-07-09 23:08:01 +0530

    Create project structure

---
+++ BloodBank.Infrastructure/DbContext/DapperContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Infrastructure.DbContext
+{
+    public class DapperContext
+    {
+        IConfiguration _configuration;
+        private readonly string? _connectionString;
+        public DapperContext(IConfiguration configuration )
+        {
+           _configuration = configuration;
+            _connectionString = configuration.GetConnectionString("SqlConnection");
+        }
+
+        //public IDbConnection CreateConnection() 
+        //{ 
+        //    ne
+        //        }
+    }
+}
