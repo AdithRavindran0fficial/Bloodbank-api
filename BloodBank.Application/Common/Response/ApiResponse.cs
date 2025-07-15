f2650fb08f3ae1e3e647be0cb78204e7e6ef55e9
Adith <adith@ontash.net>
2025-07-15 10:14:51 +0530

    create Generic Api response class

---
+++ BloodBank.Application/Common/Response/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Application.Common.Response
+{
+    public class ApiResponse<T>
+    {
+        public string Error { get; set; }
+        public string Message { get; set; }
+        public T? Data { get; set; }
+        public int Status { get; set; }
+        public ApiResponse( string messsage , int status,T? data = default ,string error= null)
+        {
+            Error = error;
+            Message = messsage;
+            Data = data;
+            Status = status;
+
+
+        }
+
+    }
+}
