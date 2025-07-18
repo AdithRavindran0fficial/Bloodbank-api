73438bf8e648ecc94560796582c2a345826b5612
Adith <adithravindranofficial@gmail.com>
2025-07-18 11:13:55 +0530

    Created interface for Regestration of Donor

---
+++ BloodBank.Application/Interfaces/IRepository/IAuthenticationRepo/IDonorRepo/IDonorRegistration.cs
@@ -0,0 +1,14 @@
+﻿using BloodBank.Application.DTOs.AuthenticationDTO.DonorDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Application.Interfaces.IRepository.IAuthenticationRepo.IDonorRepo
+{
+    public  interface IDonorRegistration
+    {
+        Task<bool> RegisterDonor(DonorDto donor);
+    }
+}
