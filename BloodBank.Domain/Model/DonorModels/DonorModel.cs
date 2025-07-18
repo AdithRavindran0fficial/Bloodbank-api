6e414abbfaba2be1a9e12c40c519aaeaa18ae0d9
Adith <adithravindranofficial@gmail.com>
2025-07-18 11:14:15 +0530

    Create Model class with valid data of donor

--- BloodBank.Domain/Model/DonorModels/DonorModel.cs
+++ BloodBank.Domain/Model/DonorModels/DonorModel.cs
@@ -8,5 +8,15 @@ namespace BloodBank.Domain.Model.DonorModels
 {
     public  class DonorModel
     {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string ? Phone { get;set; }
+        public DateTime? Dob { get; set; }
+        public int BloodGroup {  get; set; }
+        public DateTime? LastDonation { get; set; }
+       public bool IsAllowed { get; set; }  
+        public bool IsBlocked { get; set; } 
+        public string? Password { get; set; }
+
     }
 }
