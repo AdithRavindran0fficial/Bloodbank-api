79b9b5bb7f9555f0dc4a8cf6895de13e87ce652f
Adith <adithravindranofficial@gmail.com>
2025-07-18 11:12:58 +0530

    Created donordto to take input from users

---
+++ BloodBank.Application/DTOs/AuthenticationDTO/DonorDTO/DonorDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Application.DTOs.AuthenticationDTO.DonorDTO
+{
+    public class DonorDto
+    {
+
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public DateTime? DOB { get; set; }
+        [Required]
+        public int BloodGroup { get; set; }
+        [Required]
+        [MinLength(8)]
+        public string? Password { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string ? Phone { get; set; }
+      
+    }
+}
