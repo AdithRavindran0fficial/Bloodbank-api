0acd457b95e62f05837fd447c0ac5a121d0e38e2
Adith <adithravindranofficial@gmail.com>
2025-07-19 15:12:12 +0530

    Create controller for donor registration

---
+++ BloodDonation/Controllers/Authentication Controller/Donor/DonorRegistrationController.cs
@@ -0,0 +1,34 @@
+﻿using BloodBank.Application.Common.Response;
+using BloodBank.Application.DTOs.AuthenticationDTO.DonorDTO;
+using BloodBank.Application.Interfaces.IServices.IAuthenticationService.Donor;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloodDonation.Controllers.Authentication_Controller.Donor
+{
+    [Route("api/Donor")]
+    [ApiController]
+    public class DonorRegistrationController : ControllerBase
+    {
+        private readonly IDonorRegistrationService donorRegistration;
+        public DonorRegistrationController(IDonorRegistrationService registrationService)
+        {
+            donorRegistration = registrationService;
+            
+        }
+        [HttpPost]
+        public async Task<IActionResult> RegisterDonor(DonorDto donor)
+        {
+            try
+            {
+                var result = await donorRegistration.DonorRegister(donor);
+                return Ok(result);
+
+            }catch(Exception ex)
+            {
+                return StatusCode(500,new ApiResponse<object>("Internal Server error", 500));
+
+            }
+        }
+    }
+}
