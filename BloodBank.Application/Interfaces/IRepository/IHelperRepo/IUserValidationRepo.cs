4e838a9da16d0c820d53ff2a07a986c13b1b98da
Adith <adithravindranofficial@gmail.com>
2025-07-21 13:32:32 +0530

    Create repository and Implement User validation before registration

---
+++ BloodBank.Application/Interfaces/IRepository/IHelperRepo/IUserValidationRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Application.Interfaces.IRepository.IHelperRepo
+{
+    public  interface IUserValidationRepo
+    {
+        Task<bool> IsUserExits(string UserName, string Phone);
+
+    }
+}
---
+++ BloodBank.Infrastructure/HelperRepo/UserValidationRepo.cs
@@ -0,0 +1,25 @@
+﻿using BloodBank.Application.Interfaces.IRepository.IHelperRepo;
+using BloodBank.Infrastructure.DbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Infrastructure.HelperRepo
+{
+    public class UserValidationRepo : IUserValidationRepo
+    {
+        private readonly DapperContext dbContext;
+        public UserValidationRepo(DapperContext context)
+        {
+
+            dbContext = context;
+        }
+
+        public Task<bool> IsUserExits(string UserName, string Phone)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
