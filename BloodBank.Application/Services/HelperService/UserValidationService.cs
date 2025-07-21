377de78dd7ae04a36079e3a0a4d0d98cb7ae87e2
Adith <adithravindranofficial@gmail.com>
2025-07-21 13:34:40 +0530

    Create service class for User validation before registration

---
+++ BloodBank.Application/Services/HelperService/UserValidationService.cs
@@ -0,0 +1,24 @@
+﻿using BloodBank.Application.Interfaces.IRepository.IHelperRepo;
+using BloodBank.Application.Interfaces.IServices.IHelperService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Application.Services.HelperService
+{
+    public class UserValidationService : IUserValidationService
+    {
+        IUserValidationRepo _repo;
+        public UserValidationService(IUserValidationRepo userValidation)
+        {
+            _repo = userValidation;
+        }
+
+        public Task<bool> IsUserExistsAsync(string UserName, string phone)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
