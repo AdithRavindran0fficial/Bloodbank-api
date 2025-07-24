22ce218d9c9131be0d1aada3f63362ec13892337
Adith <adithravindranofficial@gmail.com>
2025-07-24 11:13:58 +0530

    Create donor Login Repository

---
+++ BloodBank.Application/Interfaces/IRepository/IAuthenticationRepo/IDonorRepo/IDonorLoginRepo.cs
@@ -0,0 +1,15 @@
+﻿using BloodBank.Application.DTOs.AuthenticationDTO.DonorDTO;
+using BloodBank.Domain.Model.DonorModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Application.Interfaces.IRepository.IAuthenticationRepo.IDonorRepo
+{
+    public  interface IDonorLoginRepo
+    {
+        Task<DonorModel?>Login(DonorLoginDTO donor);
+    }
+}
