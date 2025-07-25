ee3720f69e78c805492a8ec61240ca4847effb91
Adith <adithravindranofficial@gmail.com>
2025-07-25 22:04:55 +0530

    Created controller for login

---
+++ BloodDonation/Controllers/Authentication Controller/Donor/DonorLoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloodDonation.Controllers.Authentication_Controller.Donor
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DonorLoginController : ControllerBase
+    {
+    }
+}
