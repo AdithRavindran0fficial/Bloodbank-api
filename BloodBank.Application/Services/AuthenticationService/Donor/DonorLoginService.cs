0e67fce5d961dc5cd056d0b0d820ba43f9a99ef3
Adith <adithravindranofficial@gmail.com>
2025-07-25 22:04:41 +0530

    Implement login functionality

--- BloodBank.Application/Services/AuthenticationService/Donor/DonorLoginService.cs
+++ BloodBank.Application/Services/AuthenticationService/Donor/DonorLoginService.cs
@@ -1,4 +1,6 @@
 ﻿using BloodBank.Application.Common.Response;
+using BloodBank.Application.DTOs.AuthenticationDTO.DonorDTO;
+using BloodBank.Application.Interfaces.IRepository.IAuthenticationRepo.IDonorRepo;
 using BloodBank.Application.Interfaces.IServices.IAuthenticationService.Donor;
 using System;
 using System.Collections.Generic;
@@ -10,14 +12,31 @@ namespace BloodBank.Application.Services.AuthenticationService.Donor
 {
     public class DonorLoginService : IDonorLoignService
     {
-        public Task<ApiResponse<object>> DonorLogin(string username, string password)
+        private readonly IDonorLoginRepo donorLogin; 
+        public DonorLoginService(IDonorLoginRepo donorLoginRepo)
+        {
+            donorLogin = donorLoginRepo;
+        }
+        public async Task<ApiResponse<object>> DonorLogin(DonorLoginDTO donor)
         {
             try
             {
+                
 
+                var result =await  donorLogin.Login(donor);
+                if (result == null)
+                {
+                    return new ApiResponse<object>("user not  found", 401, true);
+                }
+                else
+                {
+                    return new ApiResponse<object>("user  found", 200, true);
+
+                }
             }
             catch(Exception ex)
             {
+                return new ApiResponse<object>(ex.Message, 500, false);
 
             }
         }
