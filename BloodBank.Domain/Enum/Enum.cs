9c07696600eac4006bfec4b1fb8be13fffe6cb20
Adith <adithravindranofficial@gmail.com>
2025-07-27 23:13:35 +0530

    Created enum for blood group

---
+++ BloodBank.Domain/Enum/Enum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Domain.Enum
+{
+    class Enum
+    {
+        public enum BloodGroup { Opositive,Apositive, Bpositive , ABpositive ,Onegative, Anegative , Bnegative , ABnegative }
+    }
+}
