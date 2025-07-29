88de8eee4ae6092cdb070305d9775f43695ad790
Adith <adithravindranofficial@gmail.com>
2025-07-29 23:22:30 +0530

    Renamed registration class

---
+++ BloodBank.Infrastructure/AuthenticationRepo/UserRegistration.cs
@@ -0,0 +1,61 @@
+﻿using BloodBank.Application.DTOs.AuthenticationDTO.DonorDTO;
+using BloodBank.Application.Interfaces.IRepository.IAuthenticationRepo.IDonorRepo;
+using BloodBank.Application.Services.HelperService.Password_Bcrypt;
+using BloodBank.Domain.Model.DonorModels;
+using BloodBank.Infrastructure.DbContext;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Infrastructure.AuthenticationRepo
+{
+    public class UserRegistration : IDonorRegistration
+    {
+        private readonly DapperContext dapperContext;
+        private readonly IPasswordHash passwordHash;
+        public UserRegistration(DapperContext context,IPasswordHash password)
+        {
+            dapperContext = context;
+            passwordHash = password;
+        }
+        public async Task<bool> RegisterDonor(DonorDto donor)
+        {
+            try
+            {
+                using(var connection = dapperContext.CreateConnection())
+                {
+                    var Donor = new DonorModel
+                    {
+                        Name = donor.Name,
+                        Phone = donor.Phone,
+                        BloodGroup = donor.BloodGroup,
+                        Dob = donor.DOB,
+                        IsAllowed = true,
+                        IsBlocked = false,
+                        LastDonation = null,
+                        Password = passwordHash.PassWordHash(donor.Password),
+                        CreatedAt = DateTime.UtcNow,
+                        RoleId = donor.RoleId
+
+                    };
+                 
+
+                 
+                    var sql = "INSERT INTO Donor VALUES (@Name, @Phone, @IsAllowed, @CreatedAt, @LastDonation, @IsBlocked, @Dob,@BloodGroup,@Password)";
+                    var result = await  connection.ExecuteAsync(sql, Donor);
+                    return result>0;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+                
+            }
+        }
+    }
+}
