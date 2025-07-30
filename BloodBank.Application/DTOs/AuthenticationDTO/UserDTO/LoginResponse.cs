a8d79fcd7d0364aac282674f0f5ece98fca60abc
Adith <adithravindranofficial@gmail.com>
2025-07-30 22:31:12 +0530

    Renamed donor to user

---
+++ BloodBank.Application/DTOs/AuthenticationDTO/UserDTO/LoginResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Application.DTOs.AuthenticationDTO.UserDTO
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+        public long Token_ExpiresIn { get; set; }
+        
+    }
+}
---
+++ BloodBank.Application/DTOs/AuthenticationDTO/UserDTO/UserDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Application.DTOs.AuthenticationDTO.DonorDTO
+{
+    public class UserDto
+    {
+
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public DateTime DOB { get; set; }
+        [Required]
+        public int BloodGroup { get; set; }
+        [Required]
+        [MinLength(8)]
+        public string? Password { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string ? Phone { get; set; }
+        public int RoleId { get; set; }
+      
+    }
+}
