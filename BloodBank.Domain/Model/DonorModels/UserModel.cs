80a01b80e394122636b0e7b153a2e732ba41a951
Adith <adithravindranofficial@gmail.com>
2025-07-30 22:33:00 +0530

    Modified models and helper class

--- BloodBank.Domain/Model/DonorModels/UserModel.cs
+++ BloodBank.Domain/Model/DonorModels/UserModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BloodBank.Domain.Model.DonorModels
 {
-    public  class DonorModel
+    public  class UserModel
     {
         public int ID { get; set; }
         public string? Name { get; set; }
