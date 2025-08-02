ee1f4da84f50c0bb89f926d9490693d9d7c76456
Adith <adithravindranofficial@gmail.com>
2025-08-02 23:29:18 +0530

    Implemented token after login

---
+++ BloodBank.Application/Services/AuthenticationService/Donor/UserLoginService.cs
@@ -0,0 +1,52 @@
+﻿using BloodBank.Application.Common.Response;
+using BloodBank.Application.DTOs.AuthenticationDTO.DonorDTO;
+using BloodBank.Application.DTOs.AuthenticationDTO.UserDTO;
+using BloodBank.Application.Interfaces.IRepository.IAuthenticationRepo.IDonorRepo;
+using BloodBank.Application.Interfaces.IServices.IAuthenticationService.Donor;
+using BloodBank.Application.Interfaces.IServices.IHelperService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Application.Services.AuthenticationService.Donor
+{
+    public class UserLoginService : IDonorLoignService
+    {
+        private readonly IUserLoginRepo donorLogin;
+        private readonly ITokenProvider tokenProvider;
+        public UserLoginService(IUserLoginRepo donorLoginRepo)
+        {
+            donorLogin = donorLoginRepo;
+        }
+        public async Task<ApiResponse<object>> DonorLogin(UserLoginDto donor)
+        {
+            try
+            {
+                
+
+                var result =await  donorLogin.Login(donor);
+                if (result == null)
+                {
+                    return new ApiResponse<object>("user not  found", 401, true);
+                }
+                else
+                {
+                    var loginres = new LoginResponse
+                    {
+                        Token = tokenProvider.GetToken(result),
+                        UserName = result.Name
+                    };
+                    return new ApiResponse<object>("user  found", 200, true,loginres);
+
+                }
+            }
+            catch(Exception ex)
+            {
+                return new ApiResponse<object>(ex.Message, 500, false);
+
+            }
+        }
+    }
+}
---
+++ BloodBank.Application/Services/AuthenticationService/Donor/UserRegistrationService.cs
@@ -0,0 +1,58 @@
+﻿using BloodBank.Application.Common.Response;
+using BloodBank.Application.DTOs.AuthenticationDTO.DonorDTO;
+using BloodBank.Application.DTOs.Base_Dtos;
+using BloodBank.Application.Interfaces.IRepository.IAuthenticationRepo.IDonorRepo;
+using BloodBank.Application.Interfaces.IServices.IAuthenticationService.Donor;
+using BloodBank.Application.Interfaces.IServices.IHelperService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Application.Services.AuthenticationService.Donor
+{
+    public class UserRegistrationService : IDonorRegistrationService
+    {
+        private readonly IUserRegistration donorRegistration;
+        private readonly IUserValidationService userValidationService;
+
+        public UserRegistrationService(IUserRegistration donor,IUserValidationService userValidation)
+        {
+            donorRegistration = donor;
+            userValidationService = userValidation;
+        }
+        public async  Task<ApiResponse<object>> DonorRegister(UserDto donor)
+        {
+            try
+            {
+                var Age = DateTime.Now.Year-donor.DOB.Year ;
+                if (donor.DOB.Date > DateTime.Now.AddYears(-Age)) Age--;
+                if (Age < 18) return new ApiResponse<object>("Your not eligible to donate", 200, false);
+                if (await userValidationService.IsUserExistsAsync(donor.Name, donor.Phone))
+                {
+
+
+                    var result = await donorRegistration.RegisterDonor(donor);
+                    if (result)
+                    {
+
+                        return new ApiResponse<object>("Registration Successful", 200, true);
+                    }
+                    return new ApiResponse<object>("Registration Failed", 200, true);
+                }
+                else
+                {
+                    return new ApiResponse<object>("User Already exist", 200, false);
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+
+            }
+        }
+    }
+}
