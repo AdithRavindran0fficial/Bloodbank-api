8dd78ed1cf763f96f79b37a3b60136d3fbf377c5
Adith <adithravindranofficial@gmail.com>
2025-08-05 22:55:26 +0530

    created login controller

---
+++ BloodDonation/Controllers/Authentication Controller/UserLoginController.cs
@@ -0,0 +1,27 @@
+﻿using BloodBank.Application.DTOs.AuthenticationDTO.DonorDTO;
+using BloodBank.Application.Interfaces.IServices.IAuthenticationService.Donor;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloodDonation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserLoginController : ControllerBase
+    {
+        private readonly IUserLoignService userLoignService;
+        public UserLoginController(IUserLoignService loginService)
+        {
+            userLoignService = loginService;
+        }
+        [HttpPost]
+        public async Task<IActionResult>LoginAsync(UserLoginDto user)
+        {
+            try
+            {
+                var result = userLoignService.DonorLogin(user);
+
+            }
+        }
+    }
+}
