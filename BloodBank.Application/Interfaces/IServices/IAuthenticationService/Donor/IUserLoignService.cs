88684c9511e28fecc6b431e852506364cdedeb12
Adith <adithravindranofficial@gmail.com>
2025-08-05 22:54:11 +0530

    Renamed file name

---
+++ BloodBank.Application/Interfaces/IServices/IAuthenticationService/Donor/IUserLoignService.cs
@@ -0,0 +1,15 @@
+﻿using BloodBank.Application.Common.Response;
+using BloodBank.Application.DTOs.AuthenticationDTO.DonorDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBank.Application.Interfaces.IServices.IAuthenticationService.Donor
+{
+    public interface IUserLoignService
+    {
+        Task<ApiResponse<Object>> DonorLogin(UserLoginDto donorLoginDTO);
+    }
+}
